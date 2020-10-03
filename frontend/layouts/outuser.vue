f7adc1154387513db7acdabd60505fd7bae59e9f
Nicolas <nikoxx9900@gmail.com>
2020-10-03 10:10:01 -0500

    Footer fix

--- frontend/layouts/outuser.vue
+++ frontend/layouts/outuser.vue
@@ -7,11 +7,6 @@
         <nuxt />
       </v-container>
     </v-main>
-    <v-footer
-      app
-    >
-      <span>&copy; {{ new Date().getFullYear() }} Formulario para cambio de contraseña - Desarrollada para ARNOProducciones por Nicolas Echeverry - Todos los derechos reservados.</span>
-    </v-footer>
   </v-app>
 </template>
 
