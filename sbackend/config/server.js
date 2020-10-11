3d7029ebf69579382633df0766fab5d938996b8d
Nicolas <nikoxx9900@gmail.com>
2020-10-11 11:36:21 -0500

    Initi Develop of Strapi Backend

---
+++ sbackend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '838e675f8d1fcdc9edee2db7b47e3593'),
+    },
+  },
+});
