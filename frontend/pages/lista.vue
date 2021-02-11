51eb079bfd4e03cb88aaca698bc3de9e97553487
Nicolas <nikoxx9900@gmail.com>
2021-02-11 16:11:03 -0500

    Mobile breackpoint on main component

--- frontend/pages/lista.vue
+++ frontend/pages/lista.vue
@@ -46,7 +46,7 @@
               loading-text="Cargando información de clientes..."
               dense
               hide-default-footer
-              mobile-breakpoint="100"
+              mobile-breakpoint="600"
               @page-count="pageCount = $event"
             >
               <!-- ########################### -->
