a758bbfef798d9fa9e82f99691d4eea85d249ff3
Nicolas <nikoxx9900@gmail.com>
2021-06-12 18:13:35 -0500

    counter url fix

--- frontend/static/counter.js
+++ frontend/static/counter.js
@@ -1,15 +1,15 @@
 const http = require('http')
 const refresh1 = () => {
-  http.get(`${process.env.BASE_URI}/refreshClientData?city=5f832e8fb0c43e2c64b37437`, function (response) {
+  http.get('http://localhost:1337/refreshClientData?city=5f832e8fb0c43e2c64b37437', function (response) {
     return response
   })
-  http.get(`${process.env.BASE_URI}/refreshClientData?city=5fc3f0408e3de73d204cd430`, function (response) {
+  http.get('http://localhost:1337/refreshClientData?city=5fc3f0408e3de73d204cd430', function (response) {
     return response
   })
-  http.get(`${process.env.BASE_URI}/refreshMikrotikData?city=5f832e8fb0c43e2c64b37437`, function (response) {
+  http.get('http://localhost:1337/refreshMikrotikData?city=5f832e8fb0c43e2c64b37437', function (response) {
     return response
   })
-  http.get(`${process.env.BASE_URI}/refreshMikrotikData?city=5fc3f0408e3de73d204cd430`, function (response) {
+  http.get('http://localhost:1337/refreshMikrotikData?city=5fc3f0408e3de73d204cd430', function (response) {
     return response
   })
 }
