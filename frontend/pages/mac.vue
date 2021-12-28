c39900317aa848124aa42e312c9b03626ec0b891
Nicolas Echeverry <nikoxx9900@gmail.com>
2021-12-28 10:52:58 -0500

    Cam scanner with locator

--- frontend/pages/mac.vue
+++ frontend/pages/mac.vue
@@ -27,24 +27,56 @@ export default {
       this.tries++
       Quagga.init({
         inputStream: {
-          name: 'Live',
           type: 'LiveStream',
-          target: document.querySelector('#camera_stream')
+          target: document.querySelector('#camera_stream'),
+          constraints: {
+            width: { min: 640 },
+            height: { min: 480 },
+            aspectRatio: { min: 1, max: 100 },
+            facingMode: 'environment' // or user
+          }
         },
+        locator: {
+          patchSize: 'medium',
+          halfSample: false
+        },
+        numOfWorkers: 4,
+        frequency: 10,
         decoder: {
-          readers: ['code_128_reader']
+          readers: [{
+            format: 'code_128_reader',
+            config: {}
+          }]
         },
         locate: true
       }, function (err) {
         if (err) {
-          return
+          console.log(err)
         }
         Quagga.start()
       })
-      Quagga.onDetected((e) => {
-        // Quagga.stop()
-        this.initialized = false
-        this.mac_result = e.codeResult.code
+      Quagga.onProcessed(function (result) {
+        const drawingCtx = Quagga.canvas.ctx.overlay
+        const drawingCanvas = Quagga.canvas.dom.overlay
+
+        if (result) {
+          if (result.boxes) {
+            drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute('width')), parseInt(drawingCanvas.getAttribute('height')))
+            result.boxes.filter(function (box) {
+              return box !== result.box
+            }).forEach(function (box) {
+              Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, { color: 'green', lineWidth: 2 })
+            })
+          }
+
+          if (result.box) {
+            Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, { color: '#00F', lineWidth: 2 })
+          }
+
+          if (result.codeResult && result.codeResult.code) {
+            Quagga.ImageDebug.drawPath(result.line, { x: 'x', y: 'y' }, drawingCtx, { color: 'red', lineWidth: 3 })
+          }
+        }
       })
     }
   }
@@ -53,7 +85,16 @@ export default {
 
 <style>
 #camera_stream {
-  width: 300px;
-  height: 300px;
+  position:relative;
+}
+canvas.drawing,
+canvas.drawingBuffer {
+    position: absolute;
+    left: 0;
+    top: 0;
+}
+#interactive.viewport > canvas, #interactive.viewport > video {
+    max-width: 100%;
+    width: 100%;
 }
 </style>
