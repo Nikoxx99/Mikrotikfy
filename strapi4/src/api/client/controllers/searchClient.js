2d2aba6e0db612d2d1481f0c83ab7550ddece548
Nicolas Echeverry <nikoxx9900@gmail.com>
2022-02-11 17:57:47 -0500

    Update searchClient.js

--- strapi4/src/api/client/controllers/searchClient.js
+++ strapi4/src/api/client/controllers/searchClient.js
@@ -11,79 +11,62 @@ module.exports = {
   async searchclient(ctx) {
     const {city, search } = ctx.query;
     if (search) { // SI LA BUSQUEDA NO ES NULA
-      if (search.length === 5) { //SI LA BUSQUEDA TIENE 5 CARACTERES
-        const toNumber = parseInt(search)
-        if (!isNaN(toNumber)) { // SI SON NUMEROS ESOS 5 CARACTERES
+      console.log(city)
           const res = await strapi.service('api::client.client').find({
-            $or: [
-              { 'city': city, 'code': { $regex: new RegExp(search, 'i') } }
-            ],
-            populate: ['city', 'plan', 'neighborhood', 'technology', 'clienttype']
-          })
-          const sanitizedEntity = await sanitize.contentAPI.output(res);
-          return { data: sanitizedEntity };
-        } else { // SI NO SON NUMEROS, PUEDEN SER UN BARRIO
-          const neighborhood = await strapi.service('api::neighborhood.neighborhood').find({ 'name': { $regex: new RegExp(search, 'i') } })
-          if (neighborhood.length > 0) { // SI ES UN BARRIO
-            const res = await strapi.service('api::client.client').find({
-              $or: [
-                { 'city': city, 'code': { $regex: new RegExp(search, 'i') } },
-                { 'city': city, 'name': { $regex: new RegExp(search, 'i') } },
-                { 'city': city, 'address': { $regex: new RegExp(search, 'i') } },
-                { 'city': city, 'neighborhood': neighborhood[0]._id },
-                { 'city': city, 'dni': { $regex: new RegExp(search, 'i') } },
-                { 'city': city, 'phone': { $regex: new RegExp(search, 'i') } }
-              ],
-              populate: ['city', 'plan', 'neighborhood', 'technology', 'clienttype']
-            })
-            const sanitizedEntity = await sanitize.contentAPI.output(res);
-          return { data: sanitizedEntity };
-          } else { // SINO ES UN BARRIO, PUEDE SER OTRO DATO
-            const res = await strapi.service('api::client.client').find({
-              $or: [
-                { 'city': city, 'code': { $regex: new RegExp(search, 'i') } },
-                { 'city': city, 'name': { $regex: new RegExp(search, 'i') } },
-                { 'city': city, 'address': { $regex: new RegExp(search, 'i') } },
-                { 'city': city, 'dni': { $regex: new RegExp(search, 'i') } },
-                { 'city': city, 'phone': { $regex: new RegExp(search, 'i') } }
-              ],
-              populate: ['city', 'plan', 'neighborhood', 'technology', 'clienttype']
-            })
-            const sanitizedEntity = await sanitize.contentAPI.output(res);
-            return { data: sanitizedEntity };
-          }
-        }
-      } else { // SI LA BUSQUEDA TIENE MENOS O MAS DE 5 CARACTERES
-        const neighborhood = await strapi.service('api::neighborhood.neighborhood').find({ 'name': { $regex: new RegExp(search, 'i') } })
-        if (neighborhood.length > 0) { // SI ES UN BARRIO
-          const res = await strapi.service('api::client.client').find({
-            $or: [
-              { 'city': city, 'code': { $regex: new RegExp(search, 'i') } },
-              { 'city': city, 'name': { $regex: new RegExp(search, 'i') } },
-              { 'city': city, 'address': { $regex: new RegExp(search, 'i') } },
-              { 'city': city, 'neighborhood': neighborhood[0]._id },
-              { 'city': city, 'dni': { $regex: new RegExp(search, 'i') } },
-              { 'city': city, 'phone': { $regex: new RegExp(search, 'i') } }
-            ],
-            populate: ['city', 'plan', 'neighborhood', 'technology', 'clienttype']
-          })
-          const sanitizedEntity = await sanitize.contentAPI.output(res);
-          return { data: sanitizedEntity };
-        } else { // SINO ES UN BARRIO, PUEDE SER OTRO DATO
-          const res = await strapi.service('api::client.client').find({
-            $or: [
-              { 'city': city, 'code': { $regex: new RegExp(search, 'i') }},
-              { 'city': city, 'name': { $regex: new RegExp(search, 'i') } },
-              { 'city': city, 'address': { $regex: new RegExp(search, 'i') } },
-              { 'city': city, 'dni': { $regex: new RegExp(search, 'i') } },
-              { 'city': city, 'phone': { $regex: new RegExp(search, 'i') } }
-            ],
+            filters: {
+              $and: [
+                {
+                  city: {
+                    name: {
+                      $eq: city
+                    }
+                  },
+                },
+                {
+                  $or: [
+                    {
+                      code: search
+                    },
+                    {
+                      neighborhood: {
+                        name: search
+                      }
+                    },
+                    {
+                      name: {
+                        $contains: search
+                      }
+                    },
+                    {
+                      address: {
+                        $contains: search
+                      }
+                    },
+                    {
+                      technology: {
+                        name: search
+                      }
+                    },
+                    {
+                      plan: {
+                        name: search
+                      }
+                    },
+                    {
+                      dni: search
+                    },
+                    {
+                      phone: search
+                    }
+                  ],
+                }
+              ],  
+            },
             populate: ['city', 'plan', 'neighborhood', 'technology', 'clienttype'],
             orderBy: { code: 'asc' }})
+            console.log(res)
           const sanitizedEntity = await sanitize.contentAPI.output(res);
           return { data: sanitizedEntity };
-        }
-      }
     } else {
       return [{ init: 'initial request' }]
     }
