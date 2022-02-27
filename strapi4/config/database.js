73325379d9c245fa51c34a0f3d57799f574bd7b6
Nicolas Echeverry <nikoxx9900@gmail.com>
2022-02-27 17:30:43 -0500

    Update database.js

--- strapi4/config/database.js
+++ strapi4/config/database.js
@@ -5,8 +5,8 @@ module.exports = ({ env }) => ({
       host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 3306),
       database: env('DATABASE_NAME', 'strapi4'),
-      user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', ''),
+      user: process.env.SQL_USER,
+      password: process.env.SQL_SECRET,
       ssl: env.bool('DATABASE_SSL', false),
     },
     // debug: true
